88e610fb3a90c5c05b587a1cc4d002e11be2e699
ethanmac <ethanmac@mac.com>
2022-03-12 23:23:57 +0800

    update

---
+++ exp_mediapipe/exp_holistic.py
@@ -0,0 +1,65 @@
+from datetime import datetime 
+
+def _track_holistic(mp=None, cv2=None):
+    if mp is None:
+        import mediapipe as mp
+    if cv2 is None:
+        import cv2 as cv2        
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_holistic = mp.solutions.holistic
+
+    cap = cv2.VideoCapture(0)
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = holistic.process(image)
+
+            # Draw landmark annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.face_landmarks,
+                mp_holistic.FACEMESH_CONTOURS,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=mp_drawing_styles
+                .get_default_face_mesh_contours_style())
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles
+                .get_default_pose_landmarks_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
+
+def do_exp():
+    d0 = datetime.now()
+    print("loading mediapipe...")
+    import mediapipe as mp
+    dt = datetime.now() - d0
+    print("loading done", "{:.2f}sec".format(dt.total_seconds()))
+
+    d0 = datetime.now()
+    import cv2 as cv2
+    dt = datetime.now() - d0
+    print("loading done", "{:.2f}sec".format(dt.total_seconds()))
+
+    _track_holistic(mp, cv2)
+
--- run_exp.py
+++ run_exp.py
@@ -1,7 +1,9 @@
 
 def main():
     #do_exp_face_mesh()
-    do_exp_selfie()
+    #do_exp_selfie()
+    #do_exp_pose()
+    do_exp_holstic()
 
 def do_exp_face_mesh():
     from exp_mediapipe.exp_face_mesh import do_exp
@@ -10,6 +12,15 @@ def do_exp_face_mesh():
 def do_exp_selfie():
     from exp_mediapipe.exp_selfie import do_exp
     do_exp()
+
+def do_exp_pose():
+    from exp_mediapipe.exp_pose import do_exp
+    do_exp()
+
+def do_exp_holstic():
+    from exp_mediapipe.exp_holistic import do_exp
+    do_exp()
+
 
 if __name__ == '__main__':
     main()
