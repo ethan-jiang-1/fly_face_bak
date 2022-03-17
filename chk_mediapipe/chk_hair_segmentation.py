ec4bcdab6fc3784906f1a42cbdb3ae4baae0820b
ethanmac <ethanmac@mac.com>
2022-03-17 15:11:37 +0800

    update

--- chk_mediapipe/chk_hair_segmentation.py
+++ chk_mediapipe/chk_hair_segmentation.py
@@ -9,7 +9,8 @@ def _create_interpeter(model_pathname):
 def _get_model_pathname():
     sub_dir = os.path.dirname(__file__)
     root_dir = os.path.dirname(sub_dir)
-    paths = [root_dir, "tflite_models", "hair_segmentation.tflite"]
+    #paths = [root_dir, "tflite_models", "hair_segmentation.tflite"]
+    paths = [root_dir, "tflite_models", "hair_segmentation_junhwanjang.tflite"]
     full_path = os.sep.join(paths)
     if not os.path.isfile(full_path):
         raise ValueError("{} not exist".format(full_path))
