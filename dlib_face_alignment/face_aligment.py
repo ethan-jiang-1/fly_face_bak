834feb9f9aec82c9de9affe2d6875f57f7e7790a
ethanmac <ethanmac@mac.com>
2022-03-26 17:33:02 +0800

    update

--- dlib_face_alignment/face_aligment.py
+++ dlib_face_alignment/face_aligment.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 13 23:09:30 2022
-
-@author: Administrator
-"""
 
 import os
 import cv2
@@ -105,7 +100,32 @@ class DlibFaceAligment():
 
 class MpFaceDetection():
     @classmethod
-    def rect_to_bb(cls, detection, width, height): # 获得人脸矩形的坐标信息
+    def create_slt_face_detection(cls):
+        import mediapipe as mp
+        mp_face_detection = mp.solutions.face_detection
+        slt_face_detection = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
+        return slt_face_detection 
+
+    @classmethod
+    def close_slt_face_detection(cls, slt_face_detection):
+        if slt_face_detection is not None:
+            slt_face_detection.close()
+
+    @classmethod
+    def get_key_dectection_result(cls, mp_face_detection_results):
+        import mediapipe as mp
+        mp_face_detection = mp.solutions.face_detection
+        if mp_face_detection_results is None:
+            return None
+        if len(mp_face_detection_results.detections) == 0:
+            return None
+
+        key_face_detection_result = mp_face_detection_results.detections[0]
+        print('Nose tip:', mp_face_detection.get_key_point(key_face_detection_result, mp_face_detection.FaceKeyPoint.NOSE_TIP))
+        return key_face_detection_result
+
+    @classmethod
+    def _rect_to_bb(cls, detection, width, height): # 获得人脸矩形的坐标信息
         x = int(detection.location_data.relative_bounding_box.xmin * width)
         y = int(detection.location_data.relative_bounding_box.ymin * height)
         w = int(detection.location_data.relative_bounding_box.width * width)
@@ -113,84 +133,99 @@ class MpFaceDetection():
         return (x, y, w, h)
 
     @classmethod
-    def rect_to_bb_expand(cls, detection, width, height, expand_ratio=0.1): # 获得人脸矩形的坐标信息
-        x = int(detection.location_data.relative_bounding_box.xmin * width)
-        y = int(detection.location_data.relative_bounding_box.ymin * height)
-        w = int(detection.location_data.relative_bounding_box.width * width)
-        h = int(detection.location_data.relative_bounding_box.height * height)
+    def _rect_to_bb_expand(cls, detection, width, height, expand_ratio=0.1): # 获得人脸矩形的坐标信息
+        (x, y, w, h) = cls._rect_to_bb(detection, width, height)
+        nx = int(x - width * expand_ratio) 
+        if nx < 0:
+            nx = 0
 
-        x = int(x - width * expand_ratio) 
-        if x < 0:
-            x = 0
+        ny = int(y - height * expand_ratio)
+        if ny < 0:
+            ny = 0
 
-        y = int(y - height * expand_ratio)
-        if y < 0:
-            y = 0
+        nw = int(w + width * expand_ratio)
+        if nx + nw > width:
+            nw = width - nx 
 
-        w = int(w + width * expand_ratio)
-        if x + w > width:
-            w = width - x 
+        nh = int(h + height * expand_ratio)
+        if ny + nh > height:
+            nh = height - ny
 
-        h = int(h + height * expand_ratio)
-        if y + h > height:
-            h = height - y
+        return (nx, ny, nw, nh)
 
-        return (x, y, w, h)
+    @classmethod
+    def draw_keypoints_detect_face(cls, annotated_image, key_face_detection_result, detected_face_bb, detected_face_bb_exp):
+        import mediapipe as mp
+        import mediapipe.python.solutions.drawing_utils as du
+        import mediapipe.python.solutions.drawing_styles as ds 
+        keypoint_drawing_spec = du.DrawingSpec(color=ds._RED, thickness=2, circle_radius=2)
+        bbox_drawing_spec = du.DrawingSpec(color=ds._PEACH, thickness=0)
+        mp.solutions.drawing_utils.draw_detection(annotated_image, key_face_detection_result, keypoint_drawing_spec, bbox_drawing_spec)
+
+    @classmethod
+    def get_cropped_img_face(cls, image, key_face_detection_result, draw_keypoints=False):
+        annotated_image = image.copy()
+        height,width = annotated_image.shape[0],annotated_image.shape[1]
+
+        detected_face_bb = cls._rect_to_bb(key_face_detection_result, width, height)
+        detected_face_bb_exp = cls._rect_to_bb_expand(key_face_detection_result, width, height, expand_ratio=0.0)
+        (nx, ny, nw, nh) = detected_face_bb_exp
+        print(detected_face_bb, detected_face_bb_exp)
+        img_detect_face = annotated_image[ny:ny+nh,nx:nx+nw]
+        #if draw_keypoints:
+        #    cls.draw_keypoints_detect_face(annotated_image, key_face_detection_result, detected_face_bb, detected_face_bb_exp)
+
+        return img_detect_face
 
 def exam_face_aligment(face_crop=True, draw_keypoints=True):
     from utils_inspect.sample_images import get_sample_images
     img_pathnames = get_sample_images()
-
-    import mediapipe as mp
-    mp_face_detection = mp.solutions.face_detection
-    mp_drawing = mp.solutions.drawing_utils
 
     def get_output_filename(filename, face_crop):
         basename = os.path.basename(filename)
         this_dir = os.path.dirname(__file__)
         root_dir = os.path.dirname(this_dir)
         if face_crop:
-            output_dir = os.sep.join([root_dir, "_reserved_fa_output"])
+            output_dir = os.sep.join([root_dir, "_reserved_output_face_aligned"])
         else:
-            output_dir = os.sep.join([root_dir, "_reserved_pa_output"])
+            output_dir = os.sep.join([root_dir, "_reserved_output_pic_aligned"])
         os.makedirs(output_dir, exist_ok= True)
         return os.sep.join([output_dir, basename])
 
-    with mp_face_detection.FaceDetection(
-        model_selection=1, min_detection_confidence=0.5) as face_detection:
-        for idx, filename in enumerate(img_pathnames):
-            t0 = time.time()
-            image = cv2.imread(filename)
+    slt_face_detection = None 
+    for idx, filename in enumerate(img_pathnames):
+        print("process {} {} face_crop: {}".format(idx, filename, face_crop))
+        t0 = time.time()
+        image = cv2.imread(filename)
 
-            if face_crop:            
-                # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
-                results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-                print('wasted time %s' %(time.time() - t0))
-                # Draw face detections of each face.
-                if not results.detections:
-                    continue
+        if face_crop:
+            if slt_face_detection is None: 
+                slt_face_detection = MpFaceDetection.create_slt_face_detection()    
 
-                annotated_image = image.copy()
-                height,width = annotated_image.shape[0],annotated_image.shape[1]
-                
-                #for detection in results.detections:
-                mp_face_detection_result = results.detections[0]
-                print('Nose tip:', mp_face_detection.get_key_point(mp_face_detection_result, mp_face_detection.FaceKeyPoint.NOSE_TIP))
+            # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+            results = slt_face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            print('wasted time %s' %(time.time() - t0))
 
-                detected_face_bb = MpFaceDetection.rect_to_bb(mp_face_detection_result, width, height)
-                #detected_face_bb = DlibFaceAligment.rect_to_bb_expand(detection, width, height)
-                (x, y, w, h) = detected_face_bb
-                img_detect_face = annotated_image[y:y+h,x:x+w]
-                if draw_keypoints:
-                    mp_drawing.draw_detection(annotated_image, mp_face_detection_result)
+            if not results.detections:
+                print("no face detected")
+                continue
 
-                aligned_pic = DlibFaceAligment.make_face_alignment(img_detect_face, draw_keypoints=draw_keypoints)
-            else:
-                aligned_pic = DlibFaceAligment.make_picture_alignment(image, draw_keypoints=draw_keypoints)
-                #face_photo = DlibFaceAligment.face_alignment(image)
-            
-            print(aligned_pic.shape, filename)
-            cv2.imwrite(get_output_filename(filename, face_crop), aligned_pic)
+            key_face_detection_result = MpFaceDetection.get_key_dectection_result(results)    
+            if key_face_detection_result is None:
+                print("no key face detected")
+                continue
+
+            img_detect_face = MpFaceDetection.get_cropped_img_face(image, key_face_detection_result, draw_keypoints=draw_keypoints)
+
+            aligned_pic = DlibFaceAligment.make_face_alignment(img_detect_face, draw_keypoints=draw_keypoints)
+        else:
+            aligned_pic = DlibFaceAligment.make_picture_alignment(image, draw_keypoints=draw_keypoints)
+        
+        output_pathname = get_output_filename(filename, face_crop)
+        cv2.imwrite(output_pathname, aligned_pic)
+        print("{} saved {}".format(output_pathname, aligned_pic.shape))
+    
+    MpFaceDetection.close_slt_face_detection(slt_face_detection)
 
 
 def _add_root_in_sys_path():
@@ -203,6 +238,6 @@ def _add_root_in_sys_path():
 
 if __name__ == '__main__':
     _add_root_in_sys_path()
-    #exam_face_aligment(face_crop=True)
-    exam_face_aligment(face_crop=False)
+    exam_face_aligment(face_crop=True)
+    #exam_face_aligment(face_crop=False)
 
