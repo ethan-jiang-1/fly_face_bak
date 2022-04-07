61a3d01e1f4e9ea5644afda9071f62e1f52d7128
EarlyAnt <54763755@qq.com>
2022-04-07 16:10:24 +0800

    1.search_matched_poster.py脚本，增加了选择图片的界面。

--- search_matched_poster.py
+++ search_matched_poster.py
@@ -1,3 +1,5 @@
+import os
+import PySimpleGUI as sg
 
 from collections import namedtuple
 from pprint import pprint
@@ -83,6 +85,45 @@ def do_exp():
     names = ["org", "hair[{}]".format(smp_ret.hair_id), "face[{}]".format(smp_ret.face_id), "beard[{}]".format(smp_ret.beard_id), "poster"]
 
     PlotHelper.plot_imgs(imgs, names)
+    
+def start_work():
+    from utils.plot_helper import PlotHelper
+    
+    sg.theme('Light Blue 2')
+    
+    layout = [[sg.Text('选择图片')],
+            [sg.Button('OK'), sg.Button('Cancel')],
+            [sg.Text('File', size=(5, 1)), sg.Input(size=(40, 1)), sg.FileBrowse(key='-File-')]
+            ]
+
+    window = sg.Window('人脸识别', layout)
+    event, values = window.read()
+
+    while True:
+        event, values = window.read()
+        print(f'Event: {event}')
+        
+        if event == 'OK':
+            if (values[0] == ""):
+                sg.popup("请选择照片");
+            elif (not os.path.exists(values[0])):
+                sg.popup("照片不存在，请重新选择")
+            else:
+                print(values[0])
+                smp_ret = SearchMatchedPoster.search_for_poster(values[0])
+                pprint(smp_ret)
+
+                img_poster = None
+                imgs = [smp_ret.img_org, smp_ret.img_hair, smp_ret.img_face, smp_ret.img_beard, img_poster]
+                names = ["org", "hair[{}]".format(smp_ret.hair_id), "face[{}]".format(smp_ret.face_id), "beard[{}]".format(smp_ret.beard_id), "poster"]
+                PlotHelper.plot_imgs(imgs, names)
+        elif event in (None, 'Cancel'):
+            break
+        print(str(values))
+        
+    window.close()
+    print(f'You clicked {event}')
 
 if __name__ == '__main__':
-    do_exp()
+    # do_exp()
+    start_work()
