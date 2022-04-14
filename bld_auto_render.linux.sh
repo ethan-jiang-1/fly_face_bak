785369655ec559846c96929767c39b468c52abd9
ethanmac <ethan1.jiang@gmail.com>
2022-04-14 18:07:38 +0800

    update

--- bld_auto_render.linux.sh
+++ bld_auto_render.linux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo 'generate posters in linux env'
+ENV_AR_MAX_VARIATION="8"
+export ENV_AR_MAX_VARIATION
+ENV_AR_COMBINATION_ID="-1"
+export ENV_AR_COMBINATION_ID
+
 ~/blender-3.0.1-linux-x64/blender -b ./bld_gen/bldmc_render_mcd_beard/render_mcd_beard.blend -P ./bld_main_auto_render.py
