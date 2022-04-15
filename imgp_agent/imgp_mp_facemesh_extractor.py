21785fff3431771e67efb0326dfb49b4aeef6928
ethanonelinux <ethan1.jiang@gmail.com>
2022-04-16 02:51:52 +0800

    temp commit

--- imgp_agent/imgp_mp_facemesh_extractor.py
+++ imgp_agent/imgp_mp_facemesh_extractor.py
@@ -2,47 +2,54 @@ from datetime import datetime
 import cv2
 import os
 import mediapipe as mp
+#import copy
 from collections import namedtuple
 
 FME_RESULT = namedtuple('FME_RESULT', "img_org img_facemesh img_facepaint img_outline mesh_results") 
 
 class ImgpFacemeshExtractor():
-    slt_facemesh = None
-    slt_reference = 0 
+    #slt_facemesh = None
+    #slt_reference = 0 
 
     @classmethod
     def init_imgp(cls):
-        cls.slt_reference += 1
-        if cls.slt_facemesh is None:
-            #from utils_inspect.inspect_solution import inspect_solution
-            mp_face_mesh = mp.solutions.face_mesh
-            cls.slt_facemesh = mp_face_mesh.FaceMesh(
-                max_num_faces=1,
-                refine_landmarks=True,
-                min_detection_confidence=0.5,
-                min_tracking_confidence=0.5) 
-            #inspect_solution(cls.slt_facemesh)
-            print("#ImgpFacemeshExtractor inited")
+        # cls.slt_reference += 1
+        # if cls.slt_facemesh is None:
+        #     #from utils_inspect.inspect_solution import inspect_solution
+        #     mp_face_mesh = mp.solutions.face_mesh
+        #     d0 = datetime.now()
+        #     cls.slt_facemesh = mp_face_mesh.FaceMesh(
+        #         max_num_faces=1,
+        #         refine_landmarks=True,
+        #         min_detection_confidence=0.5,
+        #         min_tracking_confidence=0.5) 
+        #     #inspect_solution(cls.slt_facemesh)
+        #     dt = datetime.now() - d0
+        #     print("dt", dt.total_seconds())
+        print("#ImgpFacemeshExtractor inited")
 
     @classmethod
     def close_imgp(cls):
-        cls.slt_reference -= 1
-        if cls.slt_reference == 0:
-            if cls.slt_facemesh is not None:
-                cls.slt_facemesh.close()
-                if cls.slt_facemesh is not None:
-                    del cls.slt_facemesh
-                cls.slt_facemesh = None
-            print("#ImgpFacemeshExtractor closed")
-
+        # cls.slt_reference -= 1
+        # if cls.slt_reference == 0:
+        #     if cls.slt_facemesh is not None:
+        #         cls.slt_facemesh.close()
+        #         if cls.slt_facemesh is not None:
+        #             del cls.slt_facemesh
+        #         cls.slt_facemesh = None
+        print("#ImgpFacemeshExtractor closed")
+        
     @classmethod
     def extract_mesh_features(cls, img_org, debug=False):
         d0 = datetime.now()
-        image, mesh_results = cls._extract_face_mesh(img_org, debug=debug)
+        image_bgr, mesh_results = cls._extract_face_mesh(img_org, debug=debug)
 
-        img_facemesh = cls._draw_meshes(image, mesh_results)
-        img_facepaint = cls._paint_meshes(image, mesh_results)
-        img_outline = cls._paint_outline(image, mesh_results)
+        #img_facemesh = cls._draw_meshes(image_bgr, mesh_results)
+        #img_facepaint = cls._paint_meshes(image_bgr, mesh_results)
+        #img_outline = cls._paint_outline(image_bgr, mesh_results)
+        img_facemesh = None 
+        img_facepaint = None 
+        img_outline = None 
         dt = datetime.now() - d0
         print("inference time(fm) :{:.3f}".format(dt.total_seconds()))
 
@@ -57,24 +64,32 @@ class ImgpFacemeshExtractor():
     def _extract_face_mesh(cls, img_org, debug=False):
         image = img_org.copy()
 
-        slt_facemesh = cls.slt_facemesh
-        slt_facemesh.reset()
+        #slt_facemesh = cls.slt_facemesh
+        #slt_facemesh.reset()
+        mp_face_mesh = mp.solutions.face_mesh
+        with mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as slt_facemesh:
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        d0 = datetime.now()
-        mesh_results = slt_facemesh.process(image)
-        dt = datetime.now() - d0
-        if debug:
-            print("inference time(fm): {:.3f}".format(dt.total_seconds()))
+            d0 = datetime.now()
+            mesh_results = slt_facemesh.process(image_rgb)
+            dt = datetime.now() - d0
+            if debug:
+                print("inference time(fm): {:.3f}".format(dt.total_seconds()))
 
-        # Draw the face mesh annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        return image, mesh_results
+            # Draw the face mesh annotations on the image.
+            image.flags.writeable = True
+            image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            del mesh_results
+        #return image_bgr, mesh_results
+        return image_bgr, None
 
     @classmethod
     def _draw_meshes(cls, image, mesh_results):
