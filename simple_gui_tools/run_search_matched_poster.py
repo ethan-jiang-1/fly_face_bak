9b07b8f1f7b76a65579ea61a33e151cece6f9f8c
EarlyAnt <54763755@qq.com>
2022-04-15 09:53:22 +0800

    1.imgp_search_matched_poster.py, add function [search_for_poster_with_gender] 2.run_search_matched_poster.py, add parameter [gender]

--- simple_gui_tools/run_search_matched_poster.py
+++ simple_gui_tools/run_search_matched_poster.py
@@ -40,7 +40,7 @@ def dep_one_file():
     sg.theme('Light Blue 2')
     
     layout = [[sg.Text('功能说明：识别所选照片的发型/脸型/胡子，并随机匹配卡通形象', text_color="blue")],
-              [sg.Button('OK'), sg.Button('Exit')],
+              [sg.Button('OK'), sg.Button('Exit'), sg.Radio("男", group_id=1, key="radio_male", default=True), sg.Radio("女", group_id=1, key="radio_female")],
               [sg.Text('选择文件', auto_size_text=True), sg.Input(size=(40, 1)), sg.FileBrowse(key='-File-')]
              ]
 
@@ -57,8 +57,10 @@ def dep_one_file():
             elif (not os.path.exists(values[0])):
                 sg.popup("照片不存在，请重新选择")
             else:
-                print(values[0])
-                smp_ret = SearchMatchedPoster.search_for_poster(values[0])
+                male = values["radio_male"]
+                gender = "M" if male else "F"
+                print("->file: %s, gender: %s" % (os.path.basename(values[0]), gender))
+                smp_ret = SearchMatchedPoster.search_for_poster_with_gender(values[0], gender)
                 pprint(smp_ret)
 
                 img_poster = cv2.imread(smp_ret.poster_pathname)
@@ -80,7 +82,7 @@ def dep_one_by_one():
     sg.theme('Light Blue 2')
     
     layout = [[sg.Text('功能说明：识别所选发型子目录中所有照片的发型/脸型/胡子，并随机匹配卡通形象', text_color="blue")],
-              [sg.Button('OK'), sg.Button('Exit')],
+              [sg.Button('OK'), sg.Button('Exit'), sg.Radio("男", group_id=1, key="radio_male", default=True), sg.Radio("女", group_id=1, key="radio_female")],
               [sg.Text('选择目录', auto_size_text=True), sg.Input(size=(40, 1)), sg.FolderBrowse(key='-Folder-', initial_folder=os.path.dirname(__file__))]
              ]
 
@@ -97,13 +99,16 @@ def dep_one_by_one():
             elif (not os.path.exists(values[0])):
                 sg.popup("目录不存在，请重新选择")
             else:
-                print(values[0])
+                male = values["radio_male"]
+                gender = "M" if male else "F"
+                
                 file_list = sorted(os.listdir(values[0]))
                 for file in file_list:
                     if (file.startswith(".")):
                         continue
-                    
-                    smp_ret = SearchMatchedPoster.search_for_poster("{}/{}".format(values[0], file))
+
+                    print("->file: %s, gender: %s" % (os.path.basename(values[0]), gender))
+                    smp_ret = SearchMatchedPoster.search_for_poster_with_gender("{}/{}".format(values[0], file), gender)
                     pprint(smp_ret)
 
                     img_poster = cv2.imread(smp_ret.poster_pathname)
