9ce75500d0d370211c9eb7fa3d5c1f86b9ff1b19
sunhaha123 <sunjh007@163.com>
2022-04-15 11:49:43 +0800

    update

--- bld_auto_render.linux.sh
+++ bld_auto_render.linux.sh
@@ -10,4 +10,4 @@ export ENV_AR_MAX_VARIATION
 ENV_AR_COMBINATION_ID="-1"
 export ENV_AR_COMBINATION_ID
 
-~/blender-3.0.1-linux-x64/blender -b ./bld_gen/bldmc_render_m00/render_m00.blend -P ./bld_main_auto_render.py
+~/blender/blender -b ./bld_gen/bldmc_render_m00/render_m00.blend -P ./bld_main_auto_render.py
